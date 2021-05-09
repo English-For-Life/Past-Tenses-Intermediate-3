ba4172ab77b5577ba320ad481e4791be19e8ee21
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 20:56:23 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,78 @@
+//Array of objects
+const quiz = [
+    {
+        q:"In the past, land travels _____ very long and they _____ very tiring.",
+        options:["used to take / were", "had been taking / had been", "had taken / were", "take / have been"],
+        answer: 0
+    },
+    {
+     q:"They _____ the whole building by the time the fire _____ to all the floors.",
+        options:["evacuated/had spread", "had evacuated / spread", "were evacuating / has spread", "evacuated / was spreading"],
+        answer: 1   
+    },
+    {
+        q:"Liam _____ for ten hours, but he still _____ half of the distance by then.",
+        options:["has been driving / didn't cover", "was driving / didn't cover", "had been driving / hadn't covered", "is driving / hasn't been covering"],
+        answer: 2
+    },
+    {
+        q:"I _____ to be able to concentrate on my work because I _____ enough sleep last night.",
+        options:["don't seem / haven't got", "didn't seem / am not getting", "didn't seem / don't get", "don't seem / didn't get"],
+        answer: 3
+    },
+    {
+        q:"When we _____ again, Steve and I _____ each other for more than fifteen years.",
+        options:["met/hadn't seen", "are meeting / haven't seen", "met / don't see", "have met / didn't see"],
+        answer: 0
+    },
+    {
+        q:"It was dark and cold. At one moment he thought that he _____ his way.",
+        options:["was lost", " had lost", "lost", "has lost"],
+        answer: 1
+    },
+    {
+        q:"They _____ breakfast at 7 and _____ home at eight.",
+        options:["had / leave", "had / had left", "had / left", "have / left"],
+        answer: 2
+    },
+    {
+        q:"Arthur _____ all his exams by 5 o’clock yesterday.",
+        options:["had been passing", "was passed", "passed", "had passed"],
+        answer: 3
+    },
+    {
+        q:"He evidently _____ his shoes for a very long time. They were worn-out.",
+        options:["had been wearing", "was wearing", "had been worn", "wore"],
+        answer: 0
+    },
+    {
+        q:"So, the invisible man _____ into the shop and _____ down.",
+        options:["comes/walked", "came / walked", "had came / walks", "came / had walked"],
+        answer: 1
+    },
+    {
+        q:"While the gentlemen _____ the recent events, the ladies ___ about the weather.",
+        options:["discussed / talked", "was discussing / were talking", "were discussing / were talking", "had been discussing / were talked"],
+        answer: 2
+    },
+    {
+     q:"I couldn’t imagine what _____ to her.",
+        options:["happened", "had been happening", "has been happened", "had happened"],
+        answer: 3   
+    },
+    {
+        q:"I was very tired when I arrived home. I _____ hard all day.",
+        options:["had been working", "was working", "worked", "had worked"],
+        answer: 0
+    },
+    {
+        q:"Marcus _____ for 30 years when he finally gave it up.",
+        options:["had smoked", "had been smoking", "was smoking", "smoked"],
+        answer: 1
+    },
+    {
+        q:"How long _____ you _____ when the bus finally came ?",
+        options:["were / waiting", "had / waited", "had / been waiting", "did/ wait"],
+        answer: 2
+    }
+]
